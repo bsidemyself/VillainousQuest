9ee6b5da379d320dfe8b347068b8ec6a3095d072
Dillon Tinkham <Dtink21@gmail.com>
2023-03-27 19:38:54 -0700

    Added ideas for quest parameters to Models file

--- models/index.js
+++ models/index.js
@@ -2,4 +2,51 @@ const User = require('./User');
 
 // Define sequelize associations in this file.
 
+// quest parameters:
+
+// 	Areas:
+		
+// 		Swamp
+
+// 		Classic Dungeon
+
+// 		Village
+
+// 		Forest
+
+// 		Desert
+
+// 		Tundra
+
+// 		Cave
+
+// 	Mission Type: 
+		
+// 		Siege
+		
+// 		Monster Hunt
+
+// 		Artefact Retrieval
+
+// 		Rescue
+		
+// 		Investigate
+
+// 		Escort
+
+
+// 	Number of Party Members:
+
+// 	Level of Difficulty: 
+			
+// 		Easy
+
+// 		Medium
+
+// 		Difficult
+
+	
+// 	Length (Controls how many quests are generated): 
+
+
 module.exports = { User };
