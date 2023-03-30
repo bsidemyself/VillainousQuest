ff568770707e6fcf4f9227cd5026afdde7a5ab73
sdsartor <sdsartor48@gmail.com>
2023-03-30 16:10:29 -0700

    tinkering with mysql login to fix any issues with npm.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,9 +5,9 @@ const homerouter = require('../home-router');
 const questrouter = require('./quest-router')
 
 router.use('/users', usersRouter);
-router.use('/', homerouter);
-router.use('/', commentsrouter);
-router.use('/', questrouter);
+// router.use('/', homerouter);
+// router.use('/', commentsrouter);
+// router.use('/', questrouter);
 
 
 
