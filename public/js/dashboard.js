01742e5a045dab02807324886fd21f8f18557ff7
bsidemyself <shackletron@gmail.com>
2023-04-05 22:07:53 -0700

    changes w alex

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,4 @@
+console.log("before handler")
 const newFormHandler = async (event) => {
   event.preventDefault();
 
@@ -5,39 +6,40 @@ const newFormHandler = async (event) => {
   const setting = document.querySelector('#quest-setting').value.trim();
   const challenge = document.querySelector('#quest-challenge').value.trim();
   const text = document.querySelector('#quest-text').value.trim();
-
+  console.log("before post");
   if (title && setting && challenge && text) {
-    const response = await fetch(`/quests`, {
+    console.log("before fetch");
+    const response = await fetch(`/api/quests`, {
       method: 'POST',
       body: JSON.stringify({ title, setting, challenge, text }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
-
+    console.log(response)
     if (response.ok) {
-      document.location.replace('/quests');
+      // document.location.replace('/quests');
     } else {
       alert('Failed to create quest');
     }
   }
 };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+// const delButtonHandler = async (event) => {
+//   if (event.target.hasAttribute('data-id')) {
+//     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/quests/${id}`, {
-      method: 'DELETE',
-    });
+//     const response = await fetch(`/quests/${id}`, {
+//       method: 'DELETE',
+//     });
 
-    if (response.ok) {
-      document.location.replace('/quests');
-    } else {
-      alert('Failed to delete quest');
-    }
-  }
-};
+//     if (response.ok) {
+//       document.location.replace('/quests');
+//     } else {
+//       alert('Failed to delete quest');
+//     }
+//   }
+// };
 
 document
   .querySelector('.new-quest-form')
